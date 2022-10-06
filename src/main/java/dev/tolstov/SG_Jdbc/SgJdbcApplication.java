42702ab278987e0673a15a200a294473e8820b2e
itolstov09 <itolstov09@gmail.com>
2022-10-06 21:27:42 +0600

    chore: empty project

---
+++ src/main/java/dev/tolstov/SG_Jdbc/SgJdbcApplication.java
@@ -0,0 +1,13 @@
+package dev.tolstov.SG_Jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tolstov/SG_Jdbc/SgJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tolstov.SG_Jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
